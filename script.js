905755995a7a3c81b831f4729d61756061cd572b
HarreshJ <harresh@hotmail.com>
2024-07-01 18:47:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const events = [
         { year: '27/6/24', event: 'Arrived at JFK' },
-        { year: '28/6/24', event: 'Orientation of Brooklyn - navigated via public transport: in NY the train lines are alphabetically lettered rather than being allocated names.' },
+        { year: '28/6/24', event: 'Orientation of Brooklyn - navigated via public transport: we went climbing and I learnt how to do bouldering. Furthermore, I learnt that in NY the train lines are alphabetically lettered rather than being allocated names like they are in England, and the main ones from Manhattan to Brooklyn are the J and Q lines.' },
         { year: '29/6/24', event: 'Shadowing a software engineer and watched a BRIC concert in the evening focusing on Mexican Music - I started to learn how to code in React (a framework that essentially makes coding in JS - a more core language - much easier) and I then learnt how diverse and inclusive Brooklyn is, as shown by the variety of events that they offer.' },
-        { year: '30/6/24', event: 'Visited the Smorgasburg Food Market - I tasted a variety of food from different cultures and was stunned by the aromas and strong flavours.' },
+        { year: '30/6/24', event: 'Visited the Smorgasburg Food Market where I tasted a variety of food from different cultures. For instance, I tried the American smash burger, and then a couple of Japanese bao buns. For desert, we shared a Portuguese custard tart' },
         // Add more events as needed
     ];
 
